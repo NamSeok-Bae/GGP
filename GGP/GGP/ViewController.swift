1af4c63d429e8ad6c0331bec15a5c29e1caba5a7
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2023-04-15 16:29:23 +0900

    add: 프로젝트 생성, gitignore 추가

---
+++ GGP/GGP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GGP
+//
+//  Created by 배남석 on 2023/04/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
